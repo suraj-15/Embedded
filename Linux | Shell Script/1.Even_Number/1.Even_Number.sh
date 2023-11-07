ac3cbf407bafd4b9f1a35996dad21d2c5137839e
suraj-15 <101965376+suraj-15@users.noreply.github.com>
2023-11-07 12:14:47 +0530

    Add files via upload

---
+++ Linux | Shell Script/1.Even_Number/1.Even_Number.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+<<doc
+Name : Suraj Sunil Patil
+Date : 04/11/2023
+Description : Print Even number from 1 to 100
+Sample Input :
+Sample Output :
+doc
+for i in `seq 2  2 100`
+do
+    echo " Even number : $i "
+done
